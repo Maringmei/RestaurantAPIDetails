ce65fa3309f6a1b0ab0563de855a96533dd546df
Maringmei <hadesglobizs@gmail.com>
2022-05-30 16:05:04 +0530

    30may4pm

---
+++ RestaurantAPIDetails.sh
@@ -0,0 +1,13 @@
+
+OrderList
+restaurantapp/orderlist
+
+Status Update
+https://fooddelivery.globizsapp.com/api/restaurantapp/statusupdate  -- update status Pending / Ready / Completed
+{"oid":this.orderdetails1.id, "statusid":statusid}, {"Token":this.token}
+2 - order confirm
+3 - "Order Ready for Pick Up"
+6 - Cancel
+
+History
+{"page":"0","s_date":this.s_date,"e_date":this.e_date}, {"Token":val})
